69999d2fc9bce5522b40f7dc134d5eb31485f294
Anshul889 <anshulmanaktala@gmail.com>
2019-11-21 16:58:58 +0530

    login fix

--- src/features/modals/LoginModal.jsx
+++ src/features/modals/LoginModal.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Modal, Button } from 'semantic-ui-react';
+import { Modal } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 
 import LoginForm from '../auth/Login/LoginForm';
