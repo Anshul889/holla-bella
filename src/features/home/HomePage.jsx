e3820552013c4b14f357edbd4218646566c55377
Anshul889 <anshulmanaktala@gmail.com>
2019-11-28 10:52:06 +0530

    home page almost done

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -7,6 +7,10 @@ import bell from '../../assets/Vector Smart Object.png';
 import gift from '../../assets/Vector Smart Object-2.png';
 import quality from '../../assets/Vector Smart Object-3.png';
 import delivery from '../../assets/Vector Smart Object-4.png';
+import { Button } from 'semantic-ui-react';
+import bags from '../../assets/bags2.jpg';
+import beauty from '../../assets/makeup2.jpg';
+import jewellery from '../../assets/Layer9.png';
 
 class HomePage extends Component {
   render() {
@@ -36,20 +40,37 @@ class HomePage extends Component {
           <div className={styles.outer}>
             <img src={gift} alt='gift voucher' />
             </div>
-            <h4>Gift Voucher</h4>
+            <h4>Gift<br /> Voucher</h4>
 
           </div>
           <div className={styles.icon}>
           <div className={styles.outer}>
             <img style={{transform: 'translateX(-1px)'}} src={quality} alt='quality assurace' />
             </div>
-            <h4>Quality Assurance</h4>
+            <h4>Quality<br /> Assurance</h4>
           </div>
           <div className={styles.icon}>
           <div className={styles.outer}>
             <img style={{transform: 'translateX(-1px)'}}src={bell} alt='delivery notifications' />
             </div>
             <h4>Delivery Notifications</h4>
+          </div>
+        </div>
+        <div className={styles.categories}>
+          <div style={{backgroundImage: `url(${beauty})`}} className={styles.category}>
+            <h5 >Beauty</h5>
+            <div></div>
+            <Button>SHOP NOW</Button>
+          </div>
+          <div style={{backgroundImage: `url(${jewellery})`}} className={styles.category}>
+            <h5>Jewellery</h5>
+            <div></div>
+            <Button>SHOP NOW</Button>
+          </div>
+          <div style={{backgroundImage: `url(${bags})`}} className={styles.category}>
+            <h5>Bags</h5>
+            <div></div>
+            <Button>SHOP NOW</Button>
           </div>
         </div>
         <BestOffer />
