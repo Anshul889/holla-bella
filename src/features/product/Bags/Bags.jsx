631dcfe9097bd6e5edb4b75f4ecf7c6846938f2d
Anshul889 <anshulmanaktala@gmail.com>
2019-12-19 15:41:14 +0530

    css fix

--- src/features/product/Bags/Bags.jsx
+++ src/features/product/Bags/Bags.jsx
@@ -91,7 +91,7 @@ class Bags extends Component {
     if (products.length === 0) {
       return (
         <div className={styles.bags}>
-          <img style={{ width: "100%" }} src={bagsimg} alt="bags" />
+          <img className={styles.banner} style={{ width: "100%" }} src={bagsimg} alt="bags" />
           <h1 className={styles.heading}>Bags</h1>
           <div className={styles.container}>
             <div className={styles.inner}>
@@ -144,7 +144,7 @@ class Bags extends Component {
     }
     return (
       <div className={styles.bags}>
-        <img style={{ width: "100%" }} src={bagsimg} alt="bags" />
+        <img className={styles.banner} src={bagsimg} alt="bags" />
         <h1 className={styles.heading}>Bags</h1>
         <div className={styles.container}>
           <div className={styles.filter}>
