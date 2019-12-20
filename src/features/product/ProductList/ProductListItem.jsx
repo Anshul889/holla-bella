d7e419ae369922d3da2fc53a70dcd9a80cc2a077
Anshul889 <anshulmanaktala@gmail.com>
2019-12-20 09:19:00 +0530

    small css fix

--- src/features/product/ProductList/ProductListItem.jsx
+++ src/features/product/ProductList/ProductListItem.jsx
@@ -29,8 +29,8 @@ class ProductListItem extends Component {
                         </span>
                       )}
                       <Link to={`/product/${product.id}`}>
-                        {product.price -
-                          (product.price * product.discount) / 100}{" "}
+                        {Math.round(product.price -
+                          (product.price * product.discount) / 100)}{" "}
                         KSH
                       </Link>
                       <br />
