7ffcb8e835bffb2b4a317c984df88b7b50b9987c
Anshul889 <anshulmanaktala@gmail.com>
2019-12-24 10:48:25 +0530

    add review modal

--- src/features/modals/ReviewModal.jsx
+++ src/features/modals/ReviewModal.jsx
@@ -5,16 +5,22 @@ import { closeModal } from '../modals/modalActions';
 import { Link } from "react-router-dom";
 import { objectToArray } from "../../app/common/util/helpers";
 import ProductReviewForm from '../product/ProductDetailedPage/ProductReviewForm';
+import { addReview } from '../../user/userActions';
 
 const mapState = (state) => ({
-  profile: state.firebase.profile
+  profile: state.firebase.profile,
+  auth: state.firebase.auth
 })
 
-const actions = { closeModal };
+const actions = { 
+  closeModal,
+  addReview,
+ };
 
 class ReviewModal extends Component {
   render() {
-    const {profile} = this.props;
+    const {profile, auth} = this.props;
+    const authenticated = auth.isLoaded && !auth.isEmpty;
     return (
       <Modal size='mini' open={true} onClose={this.props.closeModal}>
         <Modal.Header style={{textAlign: 'center'}}>User Review</Modal.Header>
@@ -24,7 +30,7 @@ class ReviewModal extends Component {
             <div>{objectToArray(profile.previousOrder).map(product => (
               <div key={product.id}>
                <Link to={`product/product.id`}>{product.title}</Link>
-               <ProductReviewForm />
+               <ProductReviewForm addReview={addReview} product={product} authenticated={authenticated}/>
               </div>
             ))}</div>
             <Button negative fluid content='Cancel'  onClick={closeModal}/>
